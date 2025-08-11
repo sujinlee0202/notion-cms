31db57c2a763643e4410989e667867dd7d797fe0
t2tch <sujinlee0202@gmail.com>
2025-08-11 13:14:34 +0900

    token

--- src/app/_main/page.tsx
+++ src/app/_main/page.tsx
@@ -3,7 +3,9 @@ import { NotionAPI } from "notion-client";
 import "react-notion-x/src/styles.css";
 
 export default async function NotionPage() {
-  const notion = new NotionAPI();
+  const notion = new NotionAPI({
+    authToken: process.env.NOTION_TOKEN,
+  });
   const recoredMap = await notion.getPage("247f7e819c93809db8aded1c1942022d");
   return <Notion recoredMap={recoredMap} />;
 }
--- src/app/notion/page.tsx
+++ src/app/notion/page.tsx
@@ -2,7 +2,9 @@ import NotionPopup from "@/components/NotionPopup";
 import { NotionAPI } from "notion-client";
 
 export default async function NotionPage() {
-  const notion = new NotionAPI();
+  const notion = new NotionAPI({
+    authToken: process.env.NOTION_TOKEN,
+  });
   const recoredMap = await notion.getPage("247f7e819c93809db8aded1c1942022d");
   return <NotionPopup recordMap={recoredMap} />;
 }
